8776f64a8f0ff6fa6660b246eab68eea20699c20
Miseong Kim <miseongk@naver.com>
2022-12-15 03:25:31 +0900

    feat(Ball): add Ball class

---
+++ src/main/java/baseball/domain/Ball.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Ball {
+
+    private List<Integer> ball;
+
+    public Ball(List<Integer> ball) {
+        this.ball = ball;
+    }
+}
