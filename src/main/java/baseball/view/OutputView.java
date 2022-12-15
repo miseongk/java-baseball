e8afc96075d86302230f9524429ba63ff70a43ce
Miseong Kim <miseongk@naver.com>
2022-12-15 18:59:23 +0900

    feat(OutputView): print result

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,6 +6,9 @@ public class OutputView {
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String CORRECT_NUMBER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RETRY_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
 
     public void printStartGameMessage() {
         System.out.println(START_GAME_MESSAGE);
@@ -13,6 +16,22 @@ public class OutputView {
 
     public void printInputMessage() {
         System.out.print(INPUT_NUMBER_MESSAGE);
+    }
+
+    public void printResult(int ball, int strike) {
+        String result = "";
+
+        if (ball > 0) {
+            result += ball + BALL + " ";
+        }
+        if (strike > 0) {
+            result += strike + STRIKE;
+        }
+        if (ball == 0 && strike == 0) {
+            result += NOTHING;
+        }
+
+        System.out.println(result);
     }
 
     public void printCorrectNumberMessage() {
