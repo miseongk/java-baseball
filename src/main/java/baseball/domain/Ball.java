6c59ecbc5c00b2fdf8dc1005ebff2dd04ca5ded1
Miseong Kim <miseongk@naver.com>
2022-12-15 18:09:51 +0900

    feat(Ball): calculate ball count

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -4,9 +4,28 @@ import java.util.List;
 
 public class Ball {
 
+    private final static int BALL_SIZE = 3;
+
     private List<Integer> ball;
 
     public Ball(List<Integer> ball) {
         this.ball = ball;
     }
+
+    public List<Integer> getBall() {
+        return this.ball;
+    }
+
+    public int getBallCount(List<Integer> ball, List<Integer> userBall) {
+        int ballCount = 0;
+
+        for (int i = 0; i < BALL_SIZE; i++) {
+            if (ball.contains(userBall.get(i)) && !ball.get(i).equals(userBall.get(i))) {
+                ballCount++;
+            }
+        }
+
+        return ballCount;
+    }
+
 }
