d6725253c0bf9e4d069a58d927a1ed567109de54
Miseong Kim <miseongk@naver.com>
2022-12-15 18:59:44 +0900

    feat(Validation): validate retry command is 1 or 2

--- src/main/java/baseball/domain/Validation.java
+++ src/main/java/baseball/domain/Validation.java
@@ -5,6 +5,7 @@ public class Validation {
     private static final String IS_NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
     private static final String INPUT_SIZE_ERROR_MESSAGE = "[ERROR] 3자리 숫자를 입력해주세요.";
     private static final String INPUT_NUMBER_ERROR_MESSAGE = "[ERROR] 서로 다른 3자리 숫자를 입력해주세요.";
+    private static final String INPUT_COMMAND_ERROR_MESSAGE = "[ERROR] 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public boolean isInteger(String number) {
         try {
@@ -27,6 +28,12 @@ public class Validation {
                 number.charAt(1) == number.charAt(2) ||
                 number.charAt(0) == number.charAt(2)) {
             throw new IllegalArgumentException(INPUT_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    public void validateCommand(String command) {
+        if (!(command.equals("1") || command.equals("2"))) {
+            throw new IllegalArgumentException(INPUT_COMMAND_ERROR_MESSAGE);
         }
     }
 }
