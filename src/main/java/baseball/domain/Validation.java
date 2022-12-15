c793237247e93a0456a6919670c0eb123f56bce3
Miseong Kim <miseongk@naver.com>
2022-12-15 17:31:09 +0900

    feat(Validation): validate input value

---
+++ src/main/java/baseball/domain/Validation.java
@@ -0,0 +1,32 @@
+package baseball.domain;
+
+public class Validation {
+
+    private static final String IS_NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
+    private static final String INPUT_SIZE_ERROR_MESSAGE = "[ERROR] 3자리 숫자를 입력해주세요.";
+    private static final String INPUT_NUMBER_ERROR_MESSAGE = "[ERROR] 서로 다른 3자리 숫자를 입력해주세요.";
+
+    public boolean isInteger(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(IS_NOT_INTEGER_ERROR_MESSAGE);
+        }
+
+        return true;
+    }
+
+    public void validateSize(String number) {
+        if (number.length() != 3) {
+            throw new IllegalArgumentException(INPUT_SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    public void validateDifferentNumbers(String number) {
+        if (number.charAt(0) == number.charAt(1) ||
+                number.charAt(1) == number.charAt(2) ||
+                number.charAt(0) == number.charAt(2)) {
+            throw new IllegalArgumentException(INPUT_NUMBER_ERROR_MESSAGE);
+        }
+    }
+}
