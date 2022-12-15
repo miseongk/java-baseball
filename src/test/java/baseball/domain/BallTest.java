17d027ef88cabb35da82d6d59a826ab8658f0d04
Miseong Kim <miseongk@naver.com>
2022-12-15 18:10:21 +0900

    test(Ball): ball count should be 2

---
+++ src/test/java/baseball/domain/BallTest.java
@@ -0,0 +1,26 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BallTest {
+    private Ball ball;
+
+    @BeforeEach
+    void init() {
+        ball = new Ball(List.of(1, 2, 3));
+    }
+
+    @DisplayName("볼 개수가 2여야 한다.")
+    @Test
+    void getBallCount() {
+        int ballCount = ball.getBallCount(ball.getBall(), List.of(1, 3, 2));
+        assertThat(ballCount).isEqualTo(2);
+    }
+}
